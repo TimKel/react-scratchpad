2e34d39c6ab2932e3beda81919147c1ae55dc40a
Tim Kelley <timkelley683@gmail.com>
2022-08-28 12:47:16 -0700

    adjusted App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import items from './items'
+import Alert from './Alert'
+import moreItems from './moreItems'
+import Cart from './cart'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert variant="success">
+        <h1>Welcome Back!</h1>
+      </Alert>
+      <Alert variant="warning">
+        <h1>Beware!</h1>
+      </Alert>
+      <Alert variant="danger">
+        <h1>OH NO!</h1>
+      </Alert>
+      <Cart items={items} username="carly" />
+      <Cart items={moreItems}username="rob" />
+      </>
   );
 }
 
